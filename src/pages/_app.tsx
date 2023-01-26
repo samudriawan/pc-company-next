d3a73660fbbb1a023b2f8da5763aaef065558984
Samudriawan <samudriawan@yahoo.co.id>
2023-01-26 22:56:24 +0700

    setup chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '@/styles/globals.css'
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ChakraProvider>
+			<CSSReset />
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
 }
