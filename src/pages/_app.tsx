4cc667fbfd05c90065bc9ad203731365f0f0d4ef
Samudriawan <samudriawan@yahoo.co.id>
2023-03-03 13:10:56 +0700

    add: NextAuth SessionProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,17 +6,20 @@ import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import { SkipNavLink } from '@chakra-ui/skip-nav';
 import type { AppProps } from 'next/app';
 import theme from './theme';
+import { SessionProvider } from 'next-auth/react';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider theme={theme}>
 			<CSSReset />
 			<SkipNavLink zIndex="99999">Skip to content</SkipNavLink>
-			<CartContextProvider>
-				<Navbar />
-				<Component {...pageProps} />
-				<Footer />
-			</CartContextProvider>
+			<SessionProvider session={pageProps.session}>
+				<CartContextProvider>
+					<Navbar />
+					<Component {...pageProps} />
+					<Footer />
+				</CartContextProvider>
+			</SessionProvider>
 		</ChakraProvider>
 	);
 }
